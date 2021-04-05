725041d53cc4133392497a70fb2e60e18889f7d4
Raidemonde <i.am.rahul@live.com>
2021-04-04 19:15:44 -0700

    added a coulpe more bullet points to make the functions a bit clearer

--- JS/app.js
+++ JS/app.js
@@ -1,37 +1,44 @@
-    /*
-    This is where notes should go
-    var notes = []
-    */
-    
-    /*
-    This should be a function to add darkmode
-    ColorScheme {
-        Checks Color scheme
-        if light:
-            switch to dark
-        else: 
-            switch to light
-    }
-    */
+
+// This is where notes will go, stored as a note object, returns a note
+var notesArray = []
+// This is a note object, it contains a title and a body
+var note = 
+{
+    title: "title",
+    body: "body"
+}
+
+/*
+This should be a function to add darkmode
+ColorScheme {
+    Checks Color scheme
+    if light:
+    switch to dark
+    else: 
+    switch to light
+}
+*/
 
 
-    /*
-    This should pop up the note section
-    NewNote {
-        add to notes as a body and title
-    }
-    */
+/*
+This should pop up the note section
+NewNote {
+    brings up the note area
+    brings up save/cancel button
+}
+*/
 
-    /*
-    This should save and store the note and remove the note taking area
-    SaveNote{
+/*
+This should save and store the note and remove the note taking area
+SaveNote{
+    save a note object with title and body
+}
+*/
 
-    }
-    */
-
-    /*
-    This should display notes
-    DisplayNotes {
-        Loops through notes and prints out each heading
-    }
-    */
+/*
+This should display notes
+DisplayNotes {
+    Loops through notes and prints out each heading
+    decontstructs note
+}
+*/
