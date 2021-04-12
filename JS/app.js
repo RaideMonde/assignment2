28f5c0074848abb1f1317c77c28b84c8e603acc2
Raidemonde <i.am.rahul@live.com>
2021-04-11 19:23:24 -0700

    Added the note functionality to build a list of notes

--- JS/app.js
+++ JS/app.js
@@ -1,12 +1,73 @@
 
 // This is where notes will go, stored as a note object, returns a note
-var notesArray = []
+const notesArray = []
 // This is a note object, it contains a title and a body
-var note = 
+const note = 
 {
     title: "title",
     body: "body"
 }
+
+function defineNoteWritingSection(){
+    html = `
+    <div id="note-area">
+        <div id="note" contenteditable="true"></div>
+        <button onclick="saveNote()">save</button>
+        <button onclick="cleanUp()">cancel</button>
+    </div>
+    `    
+    return html
+}
+
+function createContentWritingArea() {
+    const div = document.querySelector('#content')
+    div.insertAdjacentHTML('beforeend', defineNoteWritingSection())
+}
+
+function setCursor(){
+    const div = document.querySelector('#note')
+    div.focus()
+}
+
+
+// This should save and store the note and remove the note taking area
+function saveNote(){
+    const div = document.querySelector('#note')
+    const title = div.firstChild.textContent
+    const body = convertDivsToString()
+    notesArray.push(createNote(title, body))
+    console.log(notesArray)
+    cleanUp()
+}
+
+function createNote(title, body){
+    return{title, body}
+}
+
+function convertDivsToString(){
+    let str = ""
+    const divs = [...document.querySelectorAll('[contenteditable] > div:not(first-child)')]
+    for(const i of divs){
+        str += `${i.textContent}\n`
+    }
+    return str
+}
+
+function cleanUp(){
+    const div = document.querySelector('#note-area')
+    div.remove()
+    
+}
+
+function defineCreateNoteSection(){
+    html = `
+    <div>
+        <span onclick="createContentWritingArea()"> +Create a new note</span>
+    </div>
+    `
+    return html
+}
+
 
 /*
 This should be a function to add darkmode
@@ -28,12 +89,6 @@ NewNote {
 }
 */
 
-/*
-This should save and store the note and remove the note taking area
-SaveNote{
-    save a note object with title and body
-}
-*/
 
 /*
 This should display notes
